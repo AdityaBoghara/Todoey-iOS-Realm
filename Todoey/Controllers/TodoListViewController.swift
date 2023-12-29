941098dc0d4903a64c6cca7c29c7defeebe716ff
AdityaBoghara <bogharaaditya@gmail.com>
2023-12-29 15:31:17 -0700

    Decode data with NSCoder

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -19,26 +19,7 @@ class TodoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
-        
-        let newItem = Item()
-        newItem.title = "Pizza"
-        itemArray.append(newItem)
-        let newItem2 = Item()
-        newItem2.title = "Chocolate"
-        itemArray.append(newItem2)
-        let newItem3 = Item()
-        newItem3.title = "IceCream"
-        itemArray.append(newItem3)
-        let newItem4 = Item()
-        newItem4.title = "Pringles"
-        itemArray.append(newItem4)
-        
-        
-//        if let items = (defaults.array(forKey: "TodoListArray") as? [Item]) {
-//            itemArray = items
-//        }
+        loadItems()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -120,6 +101,23 @@ class TodoListViewController: UITableViewController {
     
     
     
+    func loadItems(){
+        
+        if let data = try? Data(contentsOf: dataFilePath!){
+            
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch{
+                print("Error decoding item, \(error)")
+            }
+            
+            
+        }
+        
+        
+    }
 
 }
 
